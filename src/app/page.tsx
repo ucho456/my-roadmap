8522460ed19e7a5a24e53016535026c99011c0a1
Yuto Araki <95033809+ucho456@users.noreply.github.com>
2023-08-17 22:00:40 +0900

    Fix deploy from github actions (#4)
    * Try deploy from CD
    
    * Try deploy from github actions
    
    * fix
    
    * save
    
    * Change version 0.1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Version 0.1.1</h1>
+      <h1>Version 0.1.2</h1>
     </main>
   );
 }
